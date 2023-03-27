e611dcdeb62d6ffda367ed8ff2e8fa5f8baeeb05
Azinus <kyzo263@gmail.com>
2023-03-27 16:34:32 +0300

    Внесены правки

--- Задача 3/main3.py
+++ Задача 3/main3.py
@@ -1,66 +1,66 @@
 import math
-class figure ():
+class Figure ():
 
     def __init__(self) -> None:
         self.area = 0
         self.perimeter = 0
 
-    def AreaCompare (self, other):
-        #if issubclass(type(other), self.__class__): # Невозможно сравнить подклассы?
-            if (self.area > other.area): return print('Первая фигура больше второй по площади')
-            elif (self.area < other.area): return print('Первая фигура меньше второй по площади')
-            elif (self.area == other.area): return print('Первая фигура равна второй по площади')
+    def area_compare (self, other) -> bool:
+        #if issubclass(type(o и функции принято называть в sther), self.__class__): # Невозможно сравнить подклассы?
+            if (self.area > other.area): return True
+            elif (self.area < other.area): return False
+            elif (self.area == other.area): return False
         #else: return print('Невозможно сравнить')
 
-    def PerimetrCompare (self, other): #не работает если не посчитать периметр заранее
+    def perimeter_compare (self, other): #не работает если не посчитать периметр заранее
         #if issubclass(other, self.__class__):
-            if (self.perimeter > other.perimeter): return print('Первая фигура больше второй по периметру')
-            elif (self.perimeter < other.perimeter): return print('Первая фигура меньше второй по периметру')
-            elif (self.perimeter == other.perimeter): return print('Первая фигура равна второй по периметру')
+            if (self.perimeter > other.perimeter): return True
+            elif (self.perimeter < other.perimeter): return False
+            elif (self.perimeter == other.perimeter): return False
         #else: return print('Невозможно сравнить')
 
-    def Area(self):
-        return print(f'Площадь фигуры: {self.area}')
+    def area_of(self) -> float:
+        return self.area
 
-    def Perimetr(self):
-        return print(f'Периметр фигуры: {self.perimeter}')
+    def perimeter_of(self) -> float:
+        return self.perimeter
 
-class square(figure):
-    def __init__(self, x) -> None:
+class Square(Figure):
+    def __init__(self, a:int) -> None:
         super().__init__()
-        self.a = x
+        self.a = a
         self.area = self.a**2
-        self.perimetr = self.a*4
+        self.perimeter = self.a*4
 
-class rectangle(square):
-    def __init__(self, x, y) -> None:
-        super().__init__(x)
-        self.b = y
+class Rectangle(Square):
+    def __init__(self, a:int, b:int) -> None:
+        super().__init__(a)
+        self.b = b
         self.area = self.a * self.b
-        self.perimetr = (self.a + self.b)*2
+        self.perimeter = (self.a + self.b)*2
 
-class triangle(figure):
-    def __init__(self, x,y,z) -> None:
+class Triangle(Figure):
+    def __init__(self, a:int, b:int, c:int) -> None:
         super().__init__()
-        self.a = x
-        self.b = y
-        self.c = z
-        self.perimetr = self.a + self.b + self.c
-        __halfprmt = self.perimetr/2
+        self.a = a
+        self.b = b
+        self.c = c
+        self.perimeter = self.a + self.b + self.c
+        __halfprmt = self.perimeter/2
         self.area = math.sqrt(__halfprmt * (__halfprmt - self.a) * (__halfprmt - self.b) * (__halfprmt - self.c)) # формула Герона
 
-class circle(figure):
+class Circle(Figure):
     def __init__(self, x) -> None:
         super().__init__()
         self.r = x
         self.area = 3.14 * self.r ** 2
-        self.perimetr = 2 * 3.14 * self.r
+        self.perimeter = 2 * 3.14 * self.r
 
-fig1 = square(4)
-fig1.Perimetr()
-fig1.Area()
-fig2 = rectangle(4, 4)
-fig1.AreaCompare(fig2)
-fig3 = circle(10)
-fig4 = triangle(4, 4, 4)
-fig3.AreaCompare(fig4)
+fig1 = Square(4)
+print(f'Периметр фигуры: {fig1.perimeter_of()}')
+print(f'Площадь фигуры: {fig1.area_of()}')
+fig2 = Rectangle(4, 4)
+print(fig1.area_compare(fig2))
+fig3 = Circle(10)
+fig4 = Triangle(4, 4, 4)
+print(fig3.area_compare(fig4))
