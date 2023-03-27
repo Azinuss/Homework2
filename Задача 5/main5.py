1a7dcd592c7755e09d68a2b098d3d25660e3cc87
Azinus <kyzo263@gmail.com>
2023-03-27 16:46:42 +0300

    Правки согласно pep8

--- Задача 5/main5.py
+++ Задача 5/main5.py
@@ -1,20 +1,20 @@
 import time 
-def timerdecor(input_func):
+def timer_decor(input_func):
     def output_func(*args):
         start = time.time()
         input_func(*args)
         print(time.time() - start)
     return output_func
 
-@timerdecor
-def summ(x,y):
+@timer_decor
+def summ(x,y)-> None:
     return print(x+y)
-@timerdecor
-def txtsumm():
+@timer_decor
+def txt_summ()-> None:
     input = open('Задача 5\input.txt','r').read().split(', ')
     output = open('Задача 5\output.txt','w')
     output.write(str(float(input[0]) + float(input[1])))
     pass
 
 summ(100, 20)
-txtsumm()
+txt_summ()
