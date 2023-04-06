fc660b9a0953a32e80320ee7027e110744d118d9
Azinus <kyzo263@gmail.com>
2023-04-07 01:30:38 +0300

    Внесены исправления

--- Задача 4/main4.py
+++ Задача 4/main4.py
@@ -1,38 +1,44 @@
-class student():
-    def __init__(self, name:str, age: int, groupnum:int, avrrating:int) -> None:
+STUDENT_PAYMENT = 6000
+GRADUATE_PAYMENT = 8000
+class Student():
+    def __init__(self, name:str, age:int, groupnum:int, avrrating:int) -> None:
         self.name = name
         self.age = age
         self.groupnum = groupnum
         self.avrrating = avrrating
-        self.paymant = 0
-        if(self.avrrating == 5): self.paymant = 6000
-        elif(self.avrrating == 4): self.paymant = 4000
-        else: self.paymant = 0
+        self.payment = 0
 
-    def showinfo (self)-> None:
+    def payment_calc(self, init_value) -> None:
+        if(self.avrrating == 5): self.payment = init_value
+        elif(self.avrrating == 4): self.payment = init_value - 2000
+        else: self.payment = 0
+        
+
+    def show_info (self)-> None:
         print('ФИО: ' + str(self.name) + '\nВозраст: ' + str(self.age))
 
-    def showstipuha (self) -> None:
-        print('Размер стипендии: ' + str(self.paymant))
+    def shows_payment (self) -> None:
+        print('Размер стипендии: ' + str(self.payment))
     
-    def cmprstipuha(self, obj)-> None:
-        if(self.paymant == obj.paymant): print('Стипендии одинаковые')
-        elif(self.paymant < obj.paymant): print('Первая стипендия меньше второй')
-        elif(self.paymant > obj.paymant): print('Первая стипендия больше второй')
+    def payment_compare(self, obj)-> None:
+        if(self.payment == obj.payment): print('Стипендии одинаковые')
+        elif(self.payment < obj.payment): print('Первая стипендия меньше второй')
+        elif(self.payment > obj.payment): print('Первая стипендия больше второй')
         else: print('Не сравнимо')
 
-class graduate(student):
+class Graduate(Student):
     def __init__(self, name:str, age:int, groupnum:int, avrrating:int, papername:str) -> None:
         super().__init__(name, age, groupnum, avrrating)
         self.paper = papername
-        if(self.avrrating == 5): self.paymant = 8000
-        elif(self.avrrating == 4): self.paymant = 6000
-        else: self.paymant = 0
 
-stud1 = student('Иван Иванович Иванов', 40, 5354208, 5)
-stud1.showinfo()
-stud1.showstipuha()
-stud2 = student('Стив Иванович Иванов', 25, 5354208, 4)
-stud1.cmprstipuha(stud2)
-stud3 = graduate('Стив Иванович Иванов', 25, 5354208, 4,'Очень умная работа')
-stud2.cmprstipuha(stud3)
+
+stud1 = Student('Иван Иванович Иванов', 40, 5354208, 5)
+stud1.payment_calc(STUDENT_PAYMENT)
+stud1.show_info()
+stud1.shows_payment()
+stud2 = Student('Стив Иванович Иванов', 25, 5354208, 4)
+stud2.payment_calc(STUDENT_PAYMENT)
+stud1.payment_compare(stud2)
+stud3 = Graduate('Стив Иванович Иванов', 25, 5354208, 4, 'Очень умная работа')
+stud3.payment_calc(GRADUATE_PAYMENT)
+stud2.payment_compare(stud3)
